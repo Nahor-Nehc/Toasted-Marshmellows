120e72320daad3c557716b22e1b924ce7443696b
Nahor Nehc <90894291+Nahor-Nehc@users.noreply.github.com>
2022-11-10 11:09:45 +0000

    adding shelve save

--- main.py
+++ main.py
@@ -4,10 +4,11 @@ from Components.attacks import *
 from Components.state import State
 from Components.transition import alpha
 import sys
+import shelve
 
 pygame.init()
 
-WIDTH, HEIGHT = 1980/2, 1080/2
+WIDTH, HEIGHT = 990, 540
 
 FPS = 60
 
@@ -25,6 +26,8 @@ DGREY =  ( 50,  50,  50)
 LBROWN = (185, 122,  87)
 DBROWN = (159, 100,  64)
 
+PADDING = WIDTH/(990/20)
+
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Toasted Marshmellows")
 
@@ -33,13 +36,15 @@ FONT = lambda x: pygame.font.SysFont("consolas.ttf", x)
 STARTTRANSITION = pygame.USEREVENT + 1
 
 
-def draw(state, marshmellows, fade):
+def draw(state, marshmellows, fade, start):
   background = pygame.Surface([WIDTH, HEIGHT])
   background.fill((255, 255, 255))
   WIN.blit(background, (0, 0))
   
+  if state.get_state() == "start":
+    start.drawText(WIN)
   
-  if state.get_state() == "menu":
+  if state.get_state() == "game":
     background.fill((255, 255, 255))
     WIN.blit(background, (0, 0))
     
@@ -49,7 +54,6 @@ def draw(state, marshmellows, fade):
 
     #draw the fade
     fade.drawRect(WIN)
-    
     fade.drawText(WIN)
 
     #if the transition has ended ...
@@ -76,6 +80,21 @@ def main():
   text = FONT(60).render("Loading...", 1, WHITE)
   fade.text("Loading...", FONT(60), position=((WIDTH - text.get_width())/2, (HEIGHT - text.get_height())/2), colour=WHITE)
   
+  start = alpha()
+  start.text("Press ENTER to start", FONT(60), position=((WIDTH - text.get_width())/2, (HEIGHT - text.get_height()-PADDING)), change = 2, colour=BLACK, repeat=True)
+  
+  #load saves
+  stored_data = shelve.open(os.path.join("Saves", "data"))
+  try:
+    _ = stored_data["do_tutorial"]
+  except KeyError:
+    stored_data["do_tutorial"] = "yes"
+    levels_completed = stored_data["levels_completed"]
+    burnt_marshmellows = stored_data["burnt_marshmellows"]
+    skewered_marshmellows = stored_data["skewered_marshmellows"]
+    
+  stored_data.close()
+  
   #initiates game loop
   run = True
   while run:
@@ -91,6 +110,12 @@ def main():
 
       #if the "x" button is pressed ...
       if event.type == pygame.QUIT:
+        
+        #save game with shelve
+        stored_data["do_tutorial"] = "no"
+        stored_data["levels_completed"] = levels_completed
+        stored_data["burnt_marshmellows"] = burnt_marshmellows
+        stored_data["skewered_marshmellows"] = skewered_marshmellows
 
         #ends game loop
         run = False
@@ -109,8 +134,8 @@ def main():
       elif event.type == pygame.KEYDOWN:
         
         if state.get_state() == "start":
-          pygame.event.post(pygame.event.Event(STARTTRANSITION))
-          print("set")
+          if event.key == pygame.K_RETURN:
+            pygame.event.post(pygame.event.Event(STARTTRANSITION))
         
       elif event.type == STARTTRANSITION:
         #starts the transition
@@ -124,6 +149,6 @@ def main():
 
     marshmellows.move(-2, 0)
     
-    draw(state, marshmellows, fade)
+    draw(state, marshmellows, fade, start)
 
 main()
